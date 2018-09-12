4d6b4e9b1a92e717987c3e5a885a1967ec42b703
Katharina Austin <Katharina Austin>
2018-09-12 15:51:59 -0400

    Removed dependencies on API Routes

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -4,14 +4,7 @@ var path = require("path");
 var fs = require("fs");
 var request = require("request");
 var cheerio = require("cheerio");
-// var MongoClient = require("mongodb").MongoClient;
-var mongodb = require("mongodb");
-// var url = "mongodb = //localhost: 27017/";
-var uri = 'mongodb://kjaustin:203980@limitlessdiscoveries.com/discoveries';
-
-var sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-        
+   
 var api = express.Router();
 
 api.use(bodyParser.json());
@@ -24,38 +17,6 @@ var title;
 var author;
 var image;
 var link;
-
-// var db = client.db("discoveries");
-var contacts = db.collection("contacts");
-var messages = db.collection("messages");
-
-api.post("/inquiry", function(req, res) {
-
-    let data = [
-        {
-            email: 'example@gmail.com',
-            message: 'You Light Up My Life',
-            firstName: 'Debby Boone'
-        }
-    ];
-
-    mongodb.MongoClient.connect(uri, function(err, client) {
-        if(err) throw err;
-
-        messages.insert(data, function(err, result) {
-            if(err) throw err;
-        });
-    });
-
-    var msg = {
-        to: 'kjuliaaustin@gmail.com',
-        from: data.email,
-        subject: 'Website Inquiry',
-        text: data.message,
-        html: '<p>' + data.message + '</p><br>' + '<p>' + data.firstName,
-    };
-    sgMail.send(msg);
-});
 
 api.get("/scrape/:level", function(req, res) {
     console.log("request received");
